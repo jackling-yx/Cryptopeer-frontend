c286770d4e653b16641e53ce7b848d39c9ddc9a1
atkinsonholly <atkinsonholly@gmail.com>
2019-02-16 16:29:04 +0000

    Listing updated

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,25 +1,33 @@
 import React from 'react'
 import '../styling/App.css';
 import { Link } from 'react-router-dom'
+const MAX_LENGTH = 8
 
 class Listing extends React.Component {
 
-    render() {
-        return(
-            <li className="card">
-                <img className="card-profile-picture" src="/images/Blank-Profile-Picture.jpg" alt="Avatar" ></img>
-                    <div className="card-container">
-                        <h4><b>{this.props.userinfo.username}</b></h4>
-                            <p>Selling: {this.props.coinString(this.props.userinfo)}</p>
-                        {this.props.coinString(this.props.userinfo) !== "None" ?
-                        <Link to="/trades/new" className="link" onClick={() => this.props.handleClick(this.props.userinfo)}>Trade</Link>
-                        : null
-                        }
+  checkTextLength = (text) => {
+    return (
+      text.length > MAX_LENGTH ?
+        `${text.substring(0, MAX_LENGTH)}...` : text
+    )
+  }
 
-                    </div>
-            </li>
-        )
-    }
+  render() {
+    return(
+      <li className="card">
+        <img className="card-profile-picture" src={this.props.userinfo.profile_pic_url !== "" ? this.props.userinfo.profile_pic_url : "/images/Blank-Profile-Picture.jpg"} alt="Avatar" ></img>
+          <div className="card-container">
+              <h4><b>{this.props.userinfo.username}</b></h4>
+                  <p>Selling: {this.checkTextLength(this.props.coinString(this.props.userinfo))}</p>
+              {this.props.coinString(this.props.userinfo) !== "None" ?
+              <Link to="/trades/new" className="link" onClick={() => this.props.handleClick(this.props.userinfo)}>Trade</Link>
+              : null
+              }
+
+          </div>
+      </li>
+    )
+  }
 }
 
 export default Listing
